cd6a6e86410e68e09df414a0504c1080b1d0f3f0
motarek122112 <mohamedtaa62@gmail.com>
2026-03-25 06:02:54 +0200

    Update train.py

--- train.py
+++ train.py
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(
 )
 
 with mlflow.start_run() as run:
-    model = RandomForestClassifier(n_estimators=1, max_depth=1, random_state=1)
+    model = RandomForestClassifier(n_estimators=10, random_state=42)
     model.fit(X_train, y_train)
 
     preds = model.predict(X_test)
